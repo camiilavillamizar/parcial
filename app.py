59df1f66b5660d9815f3e0ff60d50ae018c543e6
camiilavillamizar <camiilavillamizar@gmail.com>
2019-09-05 12:21:31 -0500

    parcial 1

--- app.py
+++ app.py
@@ -3,18 +3,24 @@ import csv
 import os
 from pathlib import Path
 from datetime import datetime
+from copy import deepcopy   #Para implementar prototype 
 
 
 app = Flask(__name__)
 
 class datos:
+    clon = 0
     def __init__(self, fch, ti, te, h):
         self.fch = fch
         self.ti = ti
         self.te = te
         self.h = h
     def __str__(self):
-        return self.ti+","+self.te+","+self.h
+        return self.fch+","+self.ti+","+self.te+","+self.h+"\n"
+
+    def clonacion(self):
+        self.clon = 1
+        return deepcopy(self)
 
 @app.route("/")
 def inicio():
@@ -24,8 +30,9 @@ def inicio():
 def index(ti, te, h):
     archivo = open("09052019.csv", "a")
     d = datos(datetime.now().strftime("%Y-%m-%d %H:%M:%S"),ti, te, h)
+    dcopy = d.clonacion()
     print(d)
-    archivo.write(str(d.fch)+" "+str(d.ti)+","+str(d.te)+","+str(d.h)+"\n");
+    archivo.write(str(dcopy.fch)+" "+str(dcopy.ti)+","+str(dcopy.te)+","+str(dcopy.h)+"\n");
     return "Dato agregado con éxito"
 
 
